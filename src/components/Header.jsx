9b0b4966452eb9fe13c224d9de9d7b29a80d3d3b
vishal kumar <vishaljpkumar4970@gmail.com>
2026-04-30 16:18:59 +0530

    new com4433

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => (
   <div className="bg-gray-900 text-white text-sm p-2 flex justify-between px-6">
-    <span>Book Online • You can request appointment 24 hours</span>
+    <span>Book Online • You can request appointment 24 hour</span>
     <span>📞 Phone: +92 89595558</span>
   </div>
 );
